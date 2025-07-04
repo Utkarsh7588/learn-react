eee0daf48579d75869928651b8fae61ae88850d1
UVM <utkarshmore7588@gmail.com>
2025-07-04 11:57:55 +0530

    basic functionality of todo

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,19 +1,32 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import { Button, TextField } from '@mui/material'
+import { Button, TextField, List, ListItem } from '@mui/material'
 
 function App() {
-
+const [inputValue , setInputValue]=useState('');
+const [list, setList]=useState<string[]>([]);
+const addToList = () => {
+  if(inputValue.trim()){
+    setList([...list,inputValue]);
+    setInputValue('');
+  }
+}
   return (
-    <>
 
+    <>
       <h1>Todo App</h1>
-      <TextField variant="outlined"  label='Todo'></TextField>
+      <TextField variant="outlined" label='Todo' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></TextField>
       <div className="card">
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={addToList}>
           Add to list
         </Button>
       </div>
+
+      <List sx={{width:'100%'}}>
+        {list.map((item, index) => (
+          <ListItem key={index}>{item}</ListItem>
+        ))}
+      </List>
     </>
   )
 }
